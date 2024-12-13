8e63f5d35a2499e7eed088f7523c9dbf66c16268
Sebastian Pavel <sebastian.v.pavel@gmail.com>
2024-12-13 16:15:14 +0200

    wip

--- actions/runQuery.ts
+++ actions/runQuery.ts
@@ -1,19 +1,34 @@
 "use server";
 
-import { createQuery } from "@/lib/queryBuilder";
 import axios from "axios";
+import { createQuery } from "queryfi";
 
 export const runQueryAction = async (type: "get" | "first") => {
   try {
-    const query = createQuery("/api/users")
+    const isLocal = process.env.NEXT_PUBLIC_NODE_ENV === "local";
+
+    console.log(
+      process.env.NEXT_PUBLIC_PLAYGROUND_URL,
+      process.env.NEXT_PUBLIC_NODE_ENV,
+      "asdasdsa"
+    );
+
+    const baseUrl = `${process.env.NEXT_PUBLIC_PLAYGROUND_URL}${
+      isLocal ? "/api" : ""
+    }`;
+
+    const query = createQuery("/api/users", {
+      baseUrl,
+    })
       .with(["posts"])
       .where({
         id: 50,
       })
       [type]();
 
-    const url = process.env.PLAYGROUND_URL + query;
-    const { data } = await axios.get(url);
+    console.log(query, "the query");
+
+    const { data } = await axios.get(query);
 
     return data;
   } catch (err) {
