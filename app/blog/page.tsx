c95786a2fbec1f9ac75841485d12a856405e06d4
Sebastian Pavel <sebastian.v.pavel@gmail.com>
2024-12-14 01:23:26 +0200

    wip

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,38 +1,34 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Author, BlogMdxFrontmatter, getAllBlogs } from "@/lib/markdown";
-import { formatDate2, stringToDate } from "@/lib/utils";
+import { Author, BlogMdxFrontmatter } from "@/lib/markdown";
+import { formatDate2 } from "@/lib/utils";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "AriaDocs - Blog",
+  title: "queryFi - Blog",
 };
 
 export default async function BlogIndexPage() {
-  const blogs = (await getAllBlogs()).sort(
-    (a, b) => stringToDate(b.date).getTime() - stringToDate(a.date).getTime()
-  );
+  // const blogs = (await getAllBlogs()).sort(
+  //   (a, b) => stringToDate(b.date).getTime() - stringToDate(a.date).getTime()
+  // );
   return (
-    <div className="w-full mx-auto flex flex-col gap-1 sm:min-h-[91vh] min-h-[88vh] pt-2">
-      <div className="mb-7 flex flex-col gap-2">
-        <h1 className="text-3xl font-extrabold">
-          The latest blogs of this product
-        </h1>
-        <p className="text-muted-foreground">
+    <div className='w-full mx-auto flex flex-col gap-1 sm:min-h-[91vh] min-h-[88vh] pt-2'>
+      <div className='flex flex-col gap-2 mb-7'>
+        <h1 className='text-3xl font-extrabold'>The latest blogs of queryFi</h1>
+        <p className='text-muted-foreground'>
           All the latest blogs and news, straight from the team.
         </p>
       </div>
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-8 gap-4 mb-5">
-        {blogs.map((blog) => (
-          <BlogCard {...blog} slug={blog.slug} key={blog.slug} />
-        ))}
+      <div className='grid grid-cols-1 gap-4 mb-5 md:grid-cols-3 sm:grid-cols-2 sm:gap-8'>
+        Nothing here yet.
       </div>
     </div>
   );
 }
 
-function BlogCard({
+export function BlogCard({
   date,
   title,
   description,
@@ -43,22 +39,22 @@ function BlogCard({
   return (
     <Link
       href={`/blog/${slug}`}
-      className="flex flex-col gap-2 items-start border rounded-md py-5 px-3 min-h-[400px]"
+      className='flex flex-col gap-2 items-start border rounded-md py-5 px-3 min-h-[400px]'
     >
-      <h3 className="text-md font-semibold -mt-1 pr-7">{title}</h3>
-      <div className="w-full">
+      <h3 className='-mt-1 font-semibold text-md pr-7'>{title}</h3>
+      <div className='w-full'>
         <Image
           src={cover}
           alt={title}
           width={400}
           height={150}
           quality={80}
-          className="w-full rounded-md object-cover h-[180px] border"
+          className='w-full rounded-md object-cover h-[180px] border'
         />
       </div>
-      <p className="text-sm text-muted-foreground">{description}</p>
-      <div className="flex items-center justify-between w-full mt-auto">
-        <p className="text-[13px] text-muted-foreground">
+      <p className='text-sm text-muted-foreground'>{description}</p>
+      <div className='flex items-center justify-between w-full mt-auto'>
+        <p className='text-[13px] text-muted-foreground'>
           Published on {formatDate2(date)}
         </p>
         <AvatarGroup users={authors} />
@@ -72,7 +68,7 @@ function AvatarGroup({ users, max = 4 }: { users: Author[]; max?: number }) {
   const remainingUsers = Math.max(users.length - max, 0);
 
   return (
-    <div className="flex items-center">
+    <div className='flex items-center'>
       {displayUsers.map((user, index) => (
         <Avatar
           key={user.username}
@@ -87,7 +83,7 @@ function AvatarGroup({ users, max = 4 }: { users: Author[]; max?: number }) {
         </Avatar>
       ))}
       {remainingUsers > 0 && (
-        <Avatar className="-ml-3 inline-block border-2 border-background hover:translate-y-1 transition-transform">
+        <Avatar className='inline-block -ml-3 transition-transform border-2 border-background hover:translate-y-1'>
           <AvatarFallback>+{remainingUsers}</AvatarFallback>
         </Avatar>
       )}
