a080fd699178b4631f14850b7b2d7fd462a02736
Sebastian Pavel <sebastian.v.pavel@gmail.com>
2024-12-16 22:05:24 +0200

    chore: make note borders rounder.

--- components/markdown/note.tsx
+++ components/markdown/note.tsx
@@ -25,11 +25,11 @@ export default function Note({
   return (
     <div
       className={cn(
-        "border rounded-md px-5 pb-0.5 mt-5 mb-7 text-sm tracking-wide",
+        "border rounded-xl px-5 pb-0.5 mt-5 mb-7 text-sm tracking-wide",
         noteClassNames
       )}
     >
-      <p className="font-bold -mb-2.5">{title}:</p> {children}
+      <p className='font-bold -mb-2.5'>{title}:</p> {children}
     </div>
   );
 }
